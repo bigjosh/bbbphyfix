c78f13d844d13be9a609f45f1c64a3793deffc84
root <root@beaglebone.localdomain>
2018-06-02 05:16:21 +0000

    User the phy register 18 bit 13 to see if network in bad

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#first install the bbbrtc command that we will needed
+#first install the bbbrtc command that we will need
 DIR=$(mktemp -d -t bbbrtc.XXXXXX) || exit 1
 echo "tmpdir = $DIR"
 pushd "$DIR"
@@ -9,6 +9,17 @@ cd bbbrtc
 make
 popd
 rm -r $DIR
+
+#next install the bbbphyreg command that we will need
+DIR=$(mktemp -d -t bbbrtc.XXXXXX) || exit 1
+echo "tmpdir = $DIR"
+pushd "$DIR"
+git clone https://github.com/bigjosh/bbbrtc
+cd bbbrtc
+make
+popd
+rm -r $DIR
+
 #remove an existing init.rc if present
 update-rc.d -f chkphy remove
 #install our script to run every boot
