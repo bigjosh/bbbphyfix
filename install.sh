e0f94de97a4796242d2c7b3a52c345923229fbfd
josh <joshremote@junk.josh.com>
2018-06-09 15:51:56 -0400

    Add comments

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#install the startup script that checks if phy problem is present
+#and reboots if it is
+
+#assumes bbbphy & phyreg are installed
+
 #remove an existing init.rc if present
 update-rc.d -f chkphy remove
 #install our script to run every boot
