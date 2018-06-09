a07b77920371d7271d30f80b2cea01c3dcf88b58
josh <joshremote@junk.josh.com>
2018-06-09 15:49:55 -0400

    New install.sh that just actually installs without pulling other repos

--- install.sh
+++ install.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-#first install the bbbrtc command that we will need
-DIR=$(mktemp -d -t bbbrtc.XXXXXX) || exit 1
-echo "tmpdir = $DIR"
-pushd "$DIR"
-git clone https://github.com/bigjosh/bbbrtc
-cd bbbrtc
-make
-popd
-rm -r $DIR
-
-#next install the bbbphyreg command that we will need
-DIR=$(mktemp -d -t bbbrtc.XXXXXX) || exit 1
-echo "tmpdir = $DIR"
-pushd "$DIR"
-git clone https://github.com/bigjosh/phyreg
-cd phyreg
-make clean
-make
-make install
-popd
-rm -r $DIR
-
 #remove an existing init.rc if present
 update-rc.d -f chkphy remove
 #install our script to run every boot
